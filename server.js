1a466677f21201908929bb14fa4b6e168ec32166
khhuyen0209-lab <khhuyen0209@gmail.com>
2026-07-20 21:04:29 +0700

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.use(express.static("public"));
 
-app.get("*", (req, res) => {
+app.get("/*splat", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
