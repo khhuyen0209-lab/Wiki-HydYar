92b000c64d1c187e7f934befd77a884e72923415
khhuyen0209-lab <khhuyen0209@gmail.com>
2026-07-20 21:00:49 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const express = require("express");
+const path = require("path");
+
 const app = express();
 
 app.use(express.static("public"));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
 
 const PORT = process.env.PORT || 3000;
 
